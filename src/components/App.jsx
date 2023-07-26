2feaef1788dcf3c8b1142ed53da45fd291f61068
Bede <110419418+Udex91@users.noreply.github.com>
2023-07-26 16:46:36 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+function App (){
+
+const [count, setCount] = useState(0);
+
+
+
+
+
+/* const [red, green, blue] = [12, 344, 4555];
+console.log(blue); */
+
+function increase(){
+ setCount(count + 1)
+ 
+}
+function decrease(){
+  setCount(count -1)
+}
+
+return(
+  <div className="container">
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
+   <button onClick={decrease}>-</button>
+   
+    
+  </div>
+);
+}
+
+  
+  
+
+
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App"
+
+let counter = 0;
+
+
+ReactDOM.render(
+ <App />,
+  document.getElementById("root")
+);
